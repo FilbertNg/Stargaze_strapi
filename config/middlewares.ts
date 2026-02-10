e112ebe02da3f00d6f461824b975db66a12d069c
FilbertNg <filbertng1303@gmail.com>
2026-02-10 13:53:09 +0700

    changing middleware exclusive to stargaze

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -6,10 +6,7 @@ export default [
     name: 'strapi::cors',
     config: {
       origin: [
-        'https://stargaze-home.netlify.app',
-        'http://localhost:3000',
-        'http://127.0.0.1:5500',
-        '*'
+        'https://stargaze-research.com'
       ],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
